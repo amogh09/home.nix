0bfba2f8527144241b94c0fb0eb378f7058aa37f
Amogh Rathore <amoghdroid09@gmail.com>
2023-02-04 20:41:01 -0800

    Add glo alias

--- home.nix
+++ home.nix
@@ -88,6 +88,7 @@
       gdc = "git diff --cached";
       gdca = "git diff --cached";
       gcmsg = "git commit -m";
+      glo = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
   };
 
