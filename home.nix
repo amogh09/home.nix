6fdc7f7d291b3a2dc3b699e659457636797adb0c
Amogh Rathore <amoghdroid09@gmail.com>
2023-02-04 20:44:11 -0800

    Add ggp alias

--- home.nix
+++ home.nix
@@ -89,6 +89,7 @@
       gdca = "git diff --cached";
       gcmsg = "git commit -m";
       glo = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      ggp = "git push origin";
     };
   };
 
